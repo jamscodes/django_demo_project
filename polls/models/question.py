37ab9b1c093a60a0d19af51754c7a39302582970
JMoore <jmoore.codes@gmail.com>
2024-02-27 10:59:46 -0600

    modularized model files

--- polls/models/question.py
+++ polls/models/question.py
@@ -13,11 +13,3 @@ class Question(models.Model):
     
     def was_published_recently(self) -> bool:
         return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
-    choice_text = models.CharField(max_length = 200)
-    votes = models.IntegerField(default = 0)
-
-    def __str__(self) -> str:
-        return self.choice_text
